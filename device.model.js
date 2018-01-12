585be3f91accfe51002b8d6a0c4bdffa84b4607a
devdeepaknagar <deepak.nagarmca@gmail.com>
2018-01-12 16:31:03 +0530

    add device

---
+++ device.model.js
@@ -0,0 +1,62 @@
+/**
+ * @file(device.model.js) With Schema for device model and all the db query function 
+ * @author Shakshi Pandey <shakshi.kumari@limitlessmobile.com>
+ * @version 1.0.0
+ * @lastModifed 11-Jan-2018
+ * @lastModifedBy Shakshi
+ */
+
+import mongoose from 'mongoose';
+
+/**
+ * [DeviceSchema is used for device data validating aginst schema]
+ * @type {[type]}
+ */
+const DeviceSchema = mongoose.Schema({
+    clientId : {type: Number }, 
+    branchId: {type: String },
+    brand:{type: String },
+    regionId: {type: String },
+    assetId : {type: String },
+    deviceId:{type: String },
+    deviceType:{type: String },
+    deviceName:{type: String },
+    serialNo: {type: String },
+    simno: {type: String },
+    status:{type: String },
+    createAt:{type: Date},
+    updatedAt:{type: Date}
+}, {collection : 'device'});
+
+let DeviceModel = mongoose.model('device', DeviceSchema);
+
+/**
+ *@description [is used for getting all data of devices from db]
+ * @return {object}
+ */
+DeviceModel.getAll = () => {
+    return DeviceModel.find({});
+}
+
+/**
+ * @description [add one device to db]
+ * @param  {object}
+ * @return {[object]}
+ */
+DeviceModel.addDevice = (deviceToAdd) => {
+    return deviceToAdd.save();
+}
+
+/**
+ * @description [responsible for remove devices from db based on condition]
+ * @param  {object}
+ * @return {[object]}
+ */
+DeviceModel.removeDevice = (carName) => {
+    return DeviceModel.remove({name: carName});
+}
+
+/**
+ * @description [make used by other module]
+ */
+export default DeviceModel;
---
+++ device.service.js
@@ -0,0 +1,86 @@
+/**
+ * @file(device.service.js) All service realted to device and entry handler file after routing  
+ * @author Shakshi Pandey <shakshi.kumari@limitlessmobile.com>
+ * @version 1.0.0
+ * @lastModifed 11-Jan-2018
+ * @lastModifedBy Shakshi
+ */
+
+import Device from '../models/device.model'
+import logger from '../core/logger/app.logger'
+
+/**
+ * [service is a object ]
+ * @type {Object}
+ */
+const service = {};
+
+/**
+ * @description [with all the calculation before getAll function of model and after getAll]
+ * @param  {[object]}
+ * @param  {[object]}
+ * @return {[object]}
+ */
+service.getAll = async (req,res) =>{
+	try{
+		const device = await Device.getAll();
+        logger.info('sending all device...');
+		res.send(device);
+	}catch(err){
+		logger.error('Error in getting device- ' + err);
+		res.send('Got error in getAll');
+	}
+}
+
+/**
+ * @description [calculation before add Device to db and after adding Device ]
+ * @param  {[object]}
+ * @param  {[object]}
+ * @return {[object]}
+ */
+service.addDevice = async (req, res) => {
+    let deviceToAdd = Device({
+        name: req.body.name,
+        clientId : req.body.clientId, 
+        branchId: req.body.branchId,
+        brand: req.body.brand,
+        regionId: req.body.regionId,
+        assetId : req.body.assetId,
+        deviceId: req.body.deviceId,
+        deviceType: req.body.deviceType,
+        deviceName: req.body.deviceName,
+        serialNo: req.body.serialNo,
+        simno: req.body.simno,
+        status: req.body.status,
+        createAt: new Date()
+    });
+    try {
+        const savedDevice = await Device.addDevice(deviceToAdd);
+        logger.info('Adding device...');
+        res.send({"success":true, "code":"200", "msg":"Device added successfully","data":savedDevice});
+    }
+    catch(err) {
+        logger.error('Error in getting Device- ' + err);
+        res.send('Got error in getAll');
+    }
+}
+/**
+ * @description [calculation before delete Device to db and after delete Device]
+ * @param  {[type]}
+ * @param  {[type]}
+ * @return {[type]}
+ */
+service.deleteDevice = async (req, res) => {
+    let deviceToDelete = req.body.name;
+    try{
+        const removedDevice = await Device.removeCar(deviceToDelete);
+        logger.info('Deleted Device- ' + removedDevice);
+        res.send('Device successfully deleted');
+    }
+    catch(err) {
+        logger.error('Failed to delete Device- ' + err);
+        res.send('Delete failed..!');
+    }
+}
+
+export default service;

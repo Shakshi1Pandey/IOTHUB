4c1d7563fb35d44c3d117158fe666cbc09a6ec72
shakshi <shakshi.kumari@limitlessmobil.com>
2018-01-15 15:48:12 +0530

    solved cross origin issue

--- server.js
+++ server.js
@@ -35,10 +35,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-console.log(index)
-app.use(index);
+
 // app.use('/users', users);
+// 
 app.use(cors())
+
+app.use(function(req, res, next) {
+ res.header('Access-Control-Allow-Origin', "*");
+ res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+})
+
+app.use(index);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');

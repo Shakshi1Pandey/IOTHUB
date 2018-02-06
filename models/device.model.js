94404540dac32cf5ffbca04602845c7e7e739a22
shakshi <shakshi.kumari@limitlessmobil.com>
2018-02-06 16:50:41 +0530

    join collection device with asset assettype,region,zone,branch

--- models/device.model.js
+++ models/device.model.js
@@ -38,7 +38,73 @@ let DeviceModel = mongoose.model('device', DeviceSchema);
  * @return {object}
  */
 DeviceModel.getAll = () => {
-    return DeviceModel.find({});
+    return DeviceModel.aggregate([
+        {
+          $lookup:{
+            from:"asset",
+            localField:"assetId",
+            foreignField:"assetId",
+            as:"asset_docs"
+          }
+
+        },
+        {
+          $unwind:"$asset_docs"
+        },
+        {
+            $lookup:{
+                from:"assettype",
+                localField:"asset_docs.assetTypeId",
+                foreignField:"assetTypeId",
+                as:"assetType_docs"
+            }
+        },
+        {
+          $unwind:"$assetType_docs"
+        },
+        {
+            $lookup:{
+                from:"region",
+                localField:"regionId",
+                foreignField:"regionId",
+                as:"region_docs"
+            }
+        },
+        {
+          $unwind:"$region_docs"
+        },
+        {
+            $lookup:{
+                from:"branch",
+                localField:"branchId",
+                foreignField:"branchId",
+                as:"branch_docs"
+            }
+        },
+        {
+          $unwind:"$branch_docs"
+        },
+        {
+            $project:{
+                clientId : 1, 
+                branchId: 1,
+                branchName:"$branch_docs.branchName",
+                brand:1,
+                regionId: 1,
+                regionName:"$region_docs.regionName",
+                assetId : 1,
+                assetName:"$asset_docs.assetName",
+                assetTypeName:"$assetType_docs.assetTypeName",
+                deviceId:1,
+                deviceType:1,
+                deviceName:1,
+                serialNo: 1,
+                simno: 1
+
+            }
+        }
+    ])
+    //return DeviceModel.find({});
 }
 
 /**
